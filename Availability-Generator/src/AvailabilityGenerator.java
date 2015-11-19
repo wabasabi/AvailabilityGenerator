633df1321c6a37648ae464f1bce1360e6da22285
wabasabi <rhwabasabi@gmail.com>
2015-11-19 18:59:36 -0500

    Commit

---
+++ Availability-Generator/src/AvailabilityGenerator.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Ryan Harrington on 11/19/2015.
+ */
+public class AvailabilityGenerator {
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}

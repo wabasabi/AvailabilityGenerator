ade433d2539e347c59d022e5bb552adb5377a23f
wabasabi <rhwabasabi@gmail.com>
2015-11-19 21:51:09 -0500

    Switched from Swing to JavaFX

--- Availability-Generator/src/AvailabilityGenerator.java
+++ Availability-Generator/src/AvailabilityGenerator.java
@@ -1,26 +1,64 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 /**
- * Created by Ryan Harrington on 11/19/2015.
+ * @author Ryan Harrington
+ * Availability Generator
  */
-import javax.swing.*;
 
-public class AvailabilityGenerator {
+public class AvailabilityGenerator extends Application {
 
-    private static void createAndShowGUI() {
-        //Create and set up the window.
-        JFrame frame = new JFrame("HelloWorldSwing");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Availability Generator");
+        GridPane pane = new GridPane();
+        pane.setAlignment(Pos.CENTER);
+        pane.setHgap(10);
+        pane.setVgap(10);
+        pane.setPadding(new Insets(25, 25, 25, 25));
+        Scene scene = new Scene(pane, 1366, 768);
 
-        JLabel label = new JLabel("Availability Generator");
-        frame.getContentPane().add(label);
-        frame.setSize(1366,768);
+        Text sceneTitle = new Text("Availability Generator");
+        sceneTitle.setFont(Font.font("Arial", FontWeight.NORMAL,20));
+        pane.add(sceneTitle, 0, 0, 2, 1);
+        Label total = new Label("Name:");
+        pane.add(total, 0, 1);
+        final TextField totalField = new TextField();
+        pane.add(totalField, 1, 1);
 
-        //Display the window.
-        //frame.pack();
-        frame.setVisible(true);
+        Button submitButton = new Button("Submit");
+        HBox hbox = new HBox(10);
+        hbox.setAlignment(Pos.BOTTOM_RIGHT);
+        hbox.getChildren().add(submitButton);
+        pane.add(hbox, 1, 4);
+
+        submitButton.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent t) {
+                //
+            }
+        });
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        createAndShowGUI();
+        launch(args);
     }
 }

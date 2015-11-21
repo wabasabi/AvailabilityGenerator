d0aba069e5abea4f9f18ebbbebd408bb55ff4a64
Steve <steven.p.gantz@gmail.com>
2015-11-21 15:50:33 -0500

    Edited mailer and test

--- Availability-Generator/src/CustomJavaMailerTest.java
+++ Availability-Generator/src/CustomJavaMailerTest.java
@@ -9,10 +9,10 @@ public class CustomJavaMailerTest {
 
     @Test
     public void testBuild() throws Exception {
-        // Generate small binary file to test
+        // Generate small "binary" file to test
         byte dataToWrite[] = "BinaryTest".getBytes();
-        FileOutputStream out = new FileOutputStream("TestFile.bin");
-        out.write(dataToWrite);
+        FileOutputStream out = new FileOutputStream("TestFile.txt");
+        out.write(dataToWrite, 0, dataToWrite.length);
         out.close();
 
         // TODO - look into having user enter their own username and password to send
@@ -23,7 +23,7 @@ public class CustomJavaMailerTest {
                 .senderID("AvailabilityGen@gmail.com")
                 .username("availabilitygen@gmail.com")
                 .password("firef0x1")
-                .fileName("TestFile.bin")
+                .fileName("TestFile.txt")
                 .build();
         customJavaMailer.generateEmail();
         customJavaMailer.sendEmail();

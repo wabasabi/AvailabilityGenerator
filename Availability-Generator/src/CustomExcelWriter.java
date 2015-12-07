0bf29c601e7090013efda52443de70ff4d283054
Steve <steven.p.gantz@gmail.com>
2015-12-07 15:50:21 -0500

    Added APK and CustomExcelWriter

--- Availability-Generator/src/CustomExcelWriter.java
+++ Availability-Generator/src/CustomExcelWriter.java
@@ -1,5 +1,23 @@
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 /**
  * Created by Steven Gantz on 11/19/2015.
  */
 public class CustomExcelWriter {
+
+    /**
+     * Workbook to write to
+     */
+    private Workbook workbook;
+
+    /**
+     * General Constructor
+     */
+    CustomExcelWriter(){
+        workbook = new XSSFWorkbook();
+        Sheet availabilitySheet = workbook.createSheet("Availability");
+
+    }
 }
---
+++ Availability-Generator/src/CustomExcelWriterTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * @author Steven Gantz
+ */
+public class CustomExcelWriterTest {
+
+    @Test
+    public void testBuild(){
+
+    }
+}
